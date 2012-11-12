31983f4c6b940d729d7042cf9bc0a1e6a2556e2f
Peter Klipfel <peter@klipfel.me>
2012-11-12 16:45:09 -0700

    added ground and skybox

--- carnival.c
+++ carnival.c
@@ -70,6 +70,65 @@ void Print(const char* format , ...)
       glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,*ch++);
 }
 
+static void ground(double x,  double y,  double z,
+                   double dx, double dy, double dz,
+                   double th1,double th2,double ph, 
+                   int quads)
+{
+   float white[] = {1,1,1,1};
+   float Emission[]  = {0.0,0.0,0.01*emission,1.0};
+   glMaterialfv(GL_FRONT_AND_BACK,GL_SHININESS,shinyvec);
+   glMaterialfv(GL_FRONT_AND_BACK,GL_SPECULAR,white);
+   glMaterialfv(GL_FRONT_AND_BACK,GL_EMISSION,Emission);
+
+   //  Save transformation
+   glPushMatrix();
+   //  Offset
+   glTranslated(x,y,z);
+   glRotated(th1,ph,1,th2);
+   glScaled(dx,dy,dz);
+
+   glEnable(GL_TEXTURE_2D);
+   glTexEnvi(GL_TEXTURE_ENV,GL_TEXTURE_ENV_MODE,GL_MODULATE);
+   glColor3f(1,1,1);
+   glBindTexture(GL_TEXTURE_2D,texture[1]);
+
+   //  passenger_box
+   glNormal3f( 0,+1, 0);
+   glBegin(GL_QUADS);
+   int i, j;
+   double mul = 2/quads;
+
+   for(i=0; i < quads; i++)
+   {
+      for(j=0; j < quads; j++)
+      {
+         glTexCoord2f(mul*(i+0),mul*(j+0)); glVertex3f(-1,0,+1); //1
+         glTexCoord2f(mul*(i+1),mul*(j+0)); glVertex3f(+1,0,+1); //2
+         glTexCoord2f(mul*(i+1),mul*(j+1)); glVertex3f(+1,0,-1); //3
+         glTexCoord2f(mul*(i+0),mul*(j+1)); glVertex3f(-1,0,-1); //4
+      }
+   }
+
+   // for (i=0;i<quads;i++)
+   // {
+   //    for (j=0;j<quads;j++)
+   //    {
+   //       glTexCoord2d(mul*(i+0),mul*(j+0)); glVertex3f(5*mul*(i+0)-5,5*mul*(j+0)-5, 0);
+   //       glTexCoord2d(mul*(i+1),mul*(j+0)); glVertex3f(5*mul*(i+1)-5,5*mul*(j+0)-5, 0);
+   //       glTexCoord2d(mul*(i+1),mul*(j+1)); glVertex3f(5*mul*(i+1)-5,5*mul*(j+1)-5, 0);
+   //       glTexCoord2d(mul*(i+0),mul*(j+1)); glVertex3f(5*mul*(i+0)-5,5*mul*(j+1)-5, 0);
+   //    }
+   // }
+
+   //  End
+   glEnd();
+   //  Undo transofrmations
+   glPopMatrix();
+   glDisable(GL_TEXTURE_2D);
+
+}
+
 /*
  *  Draw a passenger_box
  *     at (x,y,z)
@@ -348,6 +407,32 @@ static void light(double x,double y,double z,double r, double start_angle, doubl
    glPopMatrix();
 }
 
+static void sky(double x,double y,double z,double r, double start_angle, double end_angle)
+{
+   const int d=5;
+   int th,ph;
+
+   //  Save transformation
+   glPushMatrix();
+   //  Offset and scale
+   glTranslated(x,y,z);
+   glScaled(r,r,r);
+   glColor3f(.7, .75, 1);
+   //  Latitude bands
+   for (ph=start_angle;ph<end_angle;ph+=d)
+   {
+      glBegin(GL_QUAD_STRIP);
+      for (th=90;th<=270;th+=d)
+      {
+         Vertex(th,ph);
+         Vertex(th,ph+d);
+      }
+      glEnd();
+   }
+   //  Undo transformations
+   glPopMatrix();
+}
+
 static void ferris_wheel(double x, double y , double z, double size)
 {
    double pi = 3.14159265358979323846;
@@ -482,7 +567,10 @@ void display()
    }
 
    // ferris_wheel(ferris1x/10, 0, -5, 1);
-   ferris_wheel(ferris2x/10, 0, 0, 1);
+   ferris_wheel(ferris2x/10, 2.4, 0, 1);
+
+   sky(-1,-1,-1,100, 90, 270);
+   ground(-1, -1, -1, 100, 0, 100, 0, 0, 0, 10);
 
    //  Draw axes
    glColor3f(1,1,1);
